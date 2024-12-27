fdc2858880aec40ac3569a6d6698761fc50115bf
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-12-28 00:01:41 +0500

    created route getAllTodo | working sucessfully

--- src/routes/todos.routes.js
+++ src/routes/todos.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { add, edit, remove } from "../controllers/todos.controllers.js";
+import { addTodo, edit, getAllTodo, remove } from "../controllers/todos.controllers.js";
 
 const router = express.Router();
 
-router.get("/todo", add)
+router.post("/todo", addTodo)
+router.get("/todo", getAllTodo)
 router.get("/edit", edit);
 router.get("/remove", remove);
 
