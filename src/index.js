e1fd4032a9c0977fb0d12efd354a04a4088f67cf
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-12-27 17:55:57 +0500

    connect db by own | created user schema

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import todoRoutes from "./routes/todos.routes.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 const app = express();
-const port = 3000;
 
-app.use('/api/v1' , todoRoutes)
+dotenv.config();
+app.use(express.json());
 
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+  });
+
+// set routes in todoRoutes
+app.use("/api/v1", todoRoutes);
+
+// Home page route
 app.get("/", (req, res) => {
-  res.send("Home")
+  res.send("Home");
   console.log("Home");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
---
+++ src/models/user.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+export default mongoose.model("Users", userSchema);
