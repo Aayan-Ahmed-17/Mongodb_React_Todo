5889abf3b756ec274706506e07bdef1635e9874e
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-12-28 00:02:37 +0500

    copied two funcs from sir | working successful

--- src/controllers/todos.controllers.js
+++ src/controllers/todos.controllers.js
@@ -1,10 +1,43 @@
 import mongoose from "mongoose";
 import Todos from "../models/todos.models.js";
 
-function add(req, res) {
-  res.send("add")
-  console.log("add");
-}
+const addTodo = async (req, res) => {
+  const { title, description } = req.body;
+
+  if (!title || !description)
+    return res.status(400).json({
+      message: "title or description is required",
+    });
+
+  try {
+    const todo = await Todos.create({
+      title,
+      description,
+    });
+
+    res.status(201).json({
+      message: "todo added successfully",
+      data: todo,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "internal server error",
+    });
+  }
+};
+
+const getAllTodo = async (req, res) => {
+  try {
+    const todos = await Todos.find({});
+    res.json({
+      data: todos,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "internal server error",
+    });
+  }
+};
 
 function edit(req, res) {
   res.send("edit");
@@ -16,5 +49,4 @@ function remove(req, res) {
   console.log("delete");
 }
 
-
-export {add , remove, edit};
+export { addTodo, getAllTodo, remove, edit };
