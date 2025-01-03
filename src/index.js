e7773b72df92e03cf3165d0ab463b2b7634fae5c
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-03 20:30:48 +0500

    test-deployment

--- src/index.js
+++ src/index.js
@@ -4,21 +4,24 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 const app = express();
 
-dotenv.config();
+// dotenv.config();
 app.use(express.json());
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MONGO DB connection failed !!! ", err);
-  });
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT, () => {
+//       console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("MONGO DB connection failed !!! ", err);
+//   });
 
 // set routes in todoRoutes
-app.use("/api/v1", todoRoutes);
+app.listen(process.env.PORT || "3000", () => {
+  console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+});
+// app.use("/api/v1", todoRoutes);
 
 // Home page route
 app.get("/", (req, res) => {
